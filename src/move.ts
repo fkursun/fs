6630077679bfd3394fc1671fd1e8bd498653d398
admin <f.kursun@azerion.com>
2020-10-12 21:59:50 +0300

    fix

--- src/move.ts
+++ src/move.ts
@@ -9,22 +9,28 @@ type List = Array<{
 }>;
 
 export default function move(list: List, source: string, destination: string): List {
-  const sourceFiles = list[0].files;
+  const sourceFiles = [...list];
   // get reference of list, so we won't corrupt original array.
   const destinationFiles = [...list];
   // get source file which will be moved.
-  const fileToBeMoved = sourceFiles.findIndex((item: { id: string }) => item.id === source);
+  let fileToBeMoved: Array<{ id: string; name: string }> = [];
+  sourceFiles.forEach((file: { files: Array<{ id: string; name: string }> }) => {
+    if (
+      fileToBeMoved.length === 0 &&
+      file.files.findIndex((item: { id: string }) => item.id === source) !== -1
+    ) {
+      fileToBeMoved = file.files.splice(
+        file.files.findIndex((item: { id: string }) => item.id === source),
+        1,
+      );
+    }
+  });
   const fileWillBePlaced = destinationFiles.findIndex(
     (item: { id: string }) => item.id === destination,
   );
   // throw an error when source file could not detect.
-  if (fileToBeMoved === -1) throw new Error('You cannot move a folder');
-  // get corresponding element from source, store it in variable and remove it from current array.
-  const sourceItem = sourceFiles.splice(
-    sourceFiles.findIndex((item: { id: string }) => item.id === source),
-    1,
-  );
+  if (fileToBeMoved.length === 0) throw new Error('You cannot move a folder');
   if (fileWillBePlaced === -1) throw new Error("You cannot specify a file as the destination'");
-  list[fileWillBePlaced].files.push(sourceItem[0]);
+  list[fileWillBePlaced].files.push(fileToBeMoved[0]);
   return list;
 }
